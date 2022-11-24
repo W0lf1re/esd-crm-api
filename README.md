06870faf5b179d627e81f810723758adc9d3ee11
Yvann <yvann.nadal@mail-esd.com>
2022-11-24 16:21:31 +0100

    toto

--- README.md
+++ README.md
@@ -1,10 +1,43 @@
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)
+![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)
+
 # Starting project
 
 ```bash
 # start containers
 docker-compose up -d
+# start node server
+npm run start:dev
 ```
+
+# Mongo doc
 
 ```bash
-
+# going into container mongo
+docker exec -ti mongo bash
+# connect to db mongo
+mongosh
 ```
+
+# TODO
+
+> create model User (email, password)
+> create userController
+> create userRouter
+
+### Features
+
+create user
+update user
+delete user
+getOne user
+getAll user
+relation user > customer
+One user can have many customers
+
+## Credentials
+
+- Developer - [Yvann Nadal](https://github.com/W0lf1re)
+- Author - [Yvann Nadal](https://github.com/W0lf1re)
--- index.js
+++ index.js
@@ -1,64 +1,25 @@
 const express = require('express')
-const { default: mongoose } = require('mongoose')
-// dotenv
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8000
+const customerRouter = require('./src/routers/customer.router')
+const invoiceRouter = require('./src/routers/invoice.router')
+const userRouter = require('./src/routers/user.router')
+const connect = require('./config/mongoose.config')
 
-// ORM Mongoose
-mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_ADDRESS}:${process.env.MONGO_PORT}`, {})
-
-const Customer = mongoose.model('Customer', { firstName: String, lastName: String })
-// const Invoices = mongoose.model('Invoices', { firstName: String, lastName: String })
+connect()
 
 app.use(express.json())
 
-// Routes Express
+// Routes express
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-// get all customers
-app.get("/customers", (req, res) => {
-    Customer.find().then((customers) => {
-        res.send(customers);
-    });
-});
-
-// get a single customer
-app.get('/customers/:id', (req, res) => {
-    Customer.findById(req.params.id).then((customer) => {
-        res.send(customer)
-    });
-});
-
-// create a customer
-app.post('/customers', (req, res) => {
-    const data = req.body
-    const customer = new Customer(data)
-    customer.save().then(dataDB => {
-        console.log(dataDB);
-        res.send(dataDB)
-    })
-})
-
-// update a customer
-app.put('/customers/:id', (req, res) => {
-    const data = req.body
-    Customer.findByIdAndUpdate(req.params.id, data).then(() => {
-        Customer.findOne({ _id: req.params.id }).then((customer) => {
-            res.send(customer)
-        });
-    });
-})
-
-// delete a customer
-app.delete('/customers/:id', (req, res) => {
-    Customer.findByIdAndRemove(req.params.id).then((customer) => {
-        res.send("customer deleted")
-    });
-})
+app.use("/api", customerRouter)
+app.use("/api", invoiceRouter)
+app.use("/api", userRouter)
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
